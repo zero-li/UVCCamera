9f54a449fa8d8cea69745beea50b8303827846d4
Lauri Methuen <lauri.methuen@gmail.com>
2021-01-15 18:06:00 +0200

    Upgrade targetSdkVersion to 29 and migrate to androidx

--- usbCameraTest8/src/androidTest/java/com/serenegiant/usbcameratest8/ExampleInstrumentedTest.java
+++ usbCameraTest8/src/androidTest/java/com/serenegiant/usbcameratest8/ExampleInstrumentedTest.java
@@ -24,8 +24,8 @@
 package com.serenegiant.usbcameratest8;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
